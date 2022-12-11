cbad89e6fa98c831b97b7340b2c90ec3d3771870
Volodymyr Titarenko <xsligvaderx@gmail.com>
2022-12-10 20:13:01 -0600

    Update task-07.js

---
+++ js/task-07.js
@@ -0,0 +1,10 @@
+const fontSizeControlRef = document.querySelector("#font-size-control");
+const resizedTextRef = document.querySelector("#text");
+
+resizedTextRef.style.fontSize = fontSizeControlRef.value + "px";
+
+const resizedTextRangeRef = (event) => {
+  resizedTextRef.style.fontSize = event.currentTarget.value + "px";
+};
+
+fontSizeControlRef.addEventListener("input", resizedTextRangeRef);
